1dfa8b6692a600ae73f38263c2c0d1bf54e85718
tj <tomisinjoseph3@gmail.com>
2021-09-17 10:16:33 +0100

    added viewing bookmarks spec

--- lib/app.rb
+++ lib/app.rb
@@ -6,9 +6,9 @@ class BookmarkManager < Sinatra::Base
     register Sinatra::Reloader
   end
   
-  # get '/' do
-  #   'Hello BookmarkManager!'
-  # end
+  get '/' do
+    'Bookmark Manager'
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
